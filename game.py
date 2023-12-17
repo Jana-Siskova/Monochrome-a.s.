181128c67c1ba88de189478540db9a19724108b3
Jana Šišková <jana.siskova.0@gmail.com>
2023-12-17 09:35:38 +0100

    new_version

--- game.py
+++ game.py
@@ -1,30 +1,32 @@
 import json
 
-def floor_text(floor):
-    with open("floors.json", encoding="utf-8") as floors_file:
-        floors = json.load(floors_file)
-    
-    return floors[floor]
+# Function that loads json file
+def load_json(json_file):
+    with open(json_file, encoding="utf-8") as file:
+        data = json.load(file)
+    return data
 
-def places_text(number):
-    with open("places.json", encoding="utf-8") as places_file:
-        places = json.load(places_file)
-    
-    return places[number]
+# Functions that loads text from json files
+def floor_text(floor, floors):
+    return floors.get(floor, "Chybne zadané patro.")
 
-def card_card_text(number_number):
-    with open("card_card.json", encoding="utf-8") as card_card_file:
-        card_card = json.load(card_card_file)
-    
-    return card_card[number_number]
+def places_text(number, places):
+    return places.get(number, "Chybne zadaná pozice.")
 
-def card_place_text(number_place):
-    with open("card_place.json", encoding="utf-8") as card_place_file:
-        card_place = json.load(card_place_file)
-    
-    return card_place[number_place]
+def card_card_text(number_number, card_combinations):
+    return card_combinations.get(number_number, "Chybne zadaná kombinace karet.")
 
-activity = input('''Jaký bude tvůj další krok?
+def card_place_text(number_place, place_combinations):
+    return place_combinations.get(number_place, "Chybne zadaná kombinace karty a místa.")
+
+
+def main():
+    floors = load_json("floors.json")
+    places = load_json("places.json")
+    card_combinations = load_json("card_card.json")
+    place_combinations = load_json("card_place.json")
+
+    activity = input('''Jaký bude tvůj další krok?
                      
 Chci prozkoumat patro. (1)
 Chci prozkoumat pozici v místnosti. (2)
@@ -33,28 +35,29 @@ Chci zkombinovat kartu dobrodružství s místem. (4)
                                     
 Tvůj výběr: ''')
 
-if activity == "1":
-    letter = input("Zadej patro: ")
-    print(floor_text(letter))
+    if activity == "1":
+        letter = input("Zadej patro: ").upper().strip()
+        print(floor_text(letter, floors))
 
-if activity == "2":
-    place = input("Zadej číslo místa: ")
-    print(places_text(place))
+    elif activity == "2":
+        place = input("Zadej číslo místa: ").strip()
+        print(places_text(place, places))
 
-if activity == "3":
-    card_card = input("Zadej kombinaci karet: ")
-    print(card_card_text(card_card))
+    elif activity == "3":
+        card_card = input("Zadej kombinaci karet: ").strip()
+        print(card_card_text(card_card, card_combinations))
 
-if activity == "4":
-    card_place = input("Zadej kombinace karty a místa: ")
-    print(card_place_text(card_place))
+    elif activity == "4":
+        card_place = input("Zadej kombinace karty a místa: ").strip()
+        print(card_place_text(card_place, place_combinations))
 
-else: 
-    print('''Byla zadaná jiná hodnota. Zadaj jednu z možností:
+    else: 
+        activity = input('''Byla zadaná jiná hodnota. Zadaj jednu z možností:
 Chci prozkoumat patro. (1)
 Chci prozkoumat pozici v místnosti. (2)
 Chci zkombinovat karty dobrodružství. (3)
 Chci zkombinovat kartu dobrodružství s místem. (4) 
-                                    
 Tvůj výběr: ''')
 
+if __name__ == "__main__":
+    main()
